b27582e0c106c556dd4805e57ded03748eebe61c
sauce <dadad1004@naver.com>
2025-07-17 15:01:38 +0900

    chore: update db migration and dockerfiles

--- user/ent/migrate/migrations/20250717060039_init.sql
+++ user/ent/migrate/migrations/20250717060039_init.sql
@@ -11,3 +11,12 @@ CREATE TABLE "public"."users" (
   "delete_after" timestamptz NULL,
   PRIMARY KEY ("id")
 );
+-- Create "sent_emails" table
+CREATE TABLE "public"."sent_emails" (
+  "id" uuid NOT NULL,
+  "email" character varying NOT NULL,
+  "sent_at" timestamptz NOT NULL,
+  "user_id" uuid NOT NULL,
+  PRIMARY KEY ("id"),
+  CONSTRAINT "sent_emails_users_sent_emails" FOREIGN KEY ("user_id") REFERENCES "public"."users" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION
+);
